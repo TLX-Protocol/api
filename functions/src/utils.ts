bfe13d3c855bbee94e1e72fd2d3e6c6d76938998
Chase Manning <chase@manning.dev>
2023-12-05 10:48:16 +0000

    add cors

--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -1,6 +1,9 @@
 import { ethers } from "ethers";
 import { codeValidChars, messageToSign } from "./constants";
 import { APIError, FunctionHandler, HTTPHandler } from "./types";
+import cors from "cors";
+
+const corsFunc = cors({ origin: true });
 
 export function createRandomString(length: number): string {
   let result = "";
@@ -28,15 +31,17 @@ export function validateParams<T>(body: any, ...keys: string[]): T {
 
 export default function wrapHandler<T>(handler: FunctionHandler<T>): HTTPHandler {
   return async function httpHandler(request, response) {
-    try {
-      const result = await handler(request);
-      response.send(result);
-    } catch (error) {
-      if (error instanceof APIError) {
-        response.status(400).send({ error: error.message });
-      } else {
-        response.status(500).send({ error: "Internal server error" });
+    corsFunc(request, response, async () => {
+      try {
+        const result = await handler(request);
+        response.send(result);
+      } catch (error) {
+        if (error instanceof APIError) {
+          response.status(400).send({ error: error.message });
+        } else {
+          response.status(500).send({ error: "Internal server error" });
+        }
       }
-    }
+    });
   };
 }
