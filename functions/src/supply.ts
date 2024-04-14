651721a71612fa8702e57fee799e94ae1a1818ae
Chase Manning <chase@manning.dev>
2024-04-14 16:12:36 +0100

    fix issues with incorrect supplys

--- functions/src/supply.ts
+++ functions/src/supply.ts
@@ -16,32 +16,21 @@ import bigintToNumber from "./helpers/bigint-to-number";
 const provider = new JsonRpcProvider(OPTIMISM_RPC);
 const erc20abi = new Interface(["function balanceOf(address) view returns (uint256)"]);
 const tlx = new Contract(TLX, erc20abi, provider);
-const lockerabi = new Interface(["function totalStaked() view returns (uint256)"]);
-const locker = new Contract(GenesisLocker, lockerabi, provider);
 
 export const getCirculatingSupply = async () => {
   const EXCLUDED_ADDRESSES = [Airdrop, Bonding, GenesisLocker, Vesting, Staker, AmmDistributor];
 
-  const [airdropBalance, bondingBalance, genesisLockerBalance, vestingBalance, stakerBalance] = await Promise.all(
-    EXCLUDED_ADDRESSES.map((address) => tlx.balanceOf(address))
-  );
+  const [airdropBalance, bondingBalance, genesisLockerBalance, vestingBalance, stakerBalance, ammBalance] =
+    await Promise.all(EXCLUDED_ADDRESSES.map((address) => tlx.balanceOf(address)));
 
-  const totalExcluded = airdropBalance + bondingBalance + genesisLockerBalance + vestingBalance + stakerBalance;
+  const totalExcluded =
+    airdropBalance + bondingBalance + genesisLockerBalance + vestingBalance + stakerBalance + ammBalance;
 
   return MAX_SUPPLY - bigintToNumber(totalExcluded);
 };
 
 export const getTotalSupply = async () => {
-  const EXCLUDED_ADDRESSES = [Airdrop, Bonding, GenesisLocker, Vesting, AmmDistributor];
-
-  const [airdropBalance, bondingBalance, genesisLockerBalance, stakerBalance, totalStaked] = await Promise.all([
-    ...EXCLUDED_ADDRESSES.map((address) => tlx.balanceOf(address)),
-    locker.totalStaked(),
-  ]);
-
-  const totalExcluded = airdropBalance + bondingBalance + genesisLockerBalance + stakerBalance - totalStaked;
-
-  return MAX_SUPPLY - bigintToNumber(BigInt(totalExcluded));
+  return Promise.resolve(MAX_SUPPLY);
 };
 
 export const getMaxSupply = async () => {
