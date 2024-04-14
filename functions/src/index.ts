9743caea333bfdb054fae3f75ec13b469a79ee89
Chase Manning <chase@manning.dev>
2024-04-14 15:53:57 +0100

    add supply endpoints

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,11 +2,43 @@ import { onRequest } from "firebase-functions/v2/https";
 import admin from "firebase-admin";
 
 import wrapHandler from "./utils";
+import { getCirculatingSupply, getMaxSupply, getTotalSupply } from "./supply";
 
 admin.initializeApp();
 
-export const example = onRequest(
+export const maxSupply = onRequest(
   wrapHandler(async () => {
-    return { message: "Hello, world!" };
+    const maxSupply_ = await getMaxSupply();
+    return { status: "success", data: { maxSupply: maxSupply_ } };
+  })
+);
+
+export const totalSupply = onRequest(
+  wrapHandler(async () => {
+    const totalSupply_ = await getTotalSupply();
+    return { status: "success", data: { totalSupply: totalSupply_ } };
+  })
+);
+
+export const circulatingSupply = onRequest(
+  wrapHandler(async () => {
+    const circulatingSupply_ = await getCirculatingSupply();
+    return { status: "success", data: { circulatingSupply: circulatingSupply_ } };
+  })
+);
+
+export const supply = onRequest(
+  wrapHandler(async () => {
+    const maxSupply_ = await getMaxSupply();
+    const totalSupply_ = await getTotalSupply();
+    const circulatingSupply_ = await getCirculatingSupply();
+    return {
+      status: "success",
+      data: {
+        maxSupply: maxSupply_,
+        totalSupply: totalSupply_,
+        circulatingSupply: circulatingSupply_,
+      },
+    };
   })
 );
