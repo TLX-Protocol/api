64a71aaf9e62de7f9a73525a93697b7add5c952b
Chase Manning <chase@manning.dev>
2024-04-14 15:53:18 +0100

    add address and rpc constants

--- functions/src/constants.ts
+++ functions/src/constants.ts
@@ -1,1 +1,23 @@
-export const EXAMPLE_CONST = "FOO";
+export const MAX_SUPPLY = 100_000_000;
+
+// Addresses
+export const AddressProvider = "0xbaa87ecc5dd76526b51ab7fd2d0c814eb967e2e2";
+export const Airdrop = "0xccf9c3162944676c524b649d2ebecac185a47fd9";
+export const Bonding = "0x0d66198914740429b760f83b0008fb6cc9324440";
+export const ChainlinkAutomation = "0x34A2758CbE5CB8DAde0DE0B419a19cc06ffB4289";
+export const GenesisLocker = "0xc068c3261522c97ff719dc97c98c63a1356fef0f";
+export const LeveragedTokenFactory = "0x26647E97d805B478B37D2D8653C41632C112aE65";
+export const LeveragedTokenHelper = "0x8F6Ae6e28E1942b4E7b1317FB6D4F43d5f778f3b";
+export const LiquidationPointsClaimer = "0x4a1eE35F7A41B72c65AC429EC92c1221Be78404c";
+export const ParameterProvider = "0xd85c61e96fd1805f0de12165ca126243617a57b0";
+export const Referrals = "0x51965a5cc9Af9ECC66afF592f2664AB40C3cfc7B";
+export const Staker = "0xc30877315f3b621a8f7bcda27819ec29429f3817";
+export const SynthetixHandler = "0x151522B291660a328132ea9D0634Ef3d09F0433b";
+export const TLX = "0xd9cc3d70e730503e7f28c1b407389198c4b75fa2";
+export const Vesting = "0xebd707214ea4bae19e826f14ec2a82d1b44a75f3";
+export const ZapSwap = "0x461aB832D11A7B18CC444B7C92a1E2B8877A327A";
+export const VelodromePool = "0x6eA20Dbc05f58ED607c374e545BFb402d5796770";
+export const AmmDistributor = "0x9d27E96B3564e51422C1f0592f42b3934f2bd056";
+
+// Endpoints
+export const OPTIMISM_RPC = "https://mainnet.optimism.io/";
