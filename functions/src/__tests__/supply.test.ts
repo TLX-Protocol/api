e2ac74db9c351881ab35a65215df8fc673db9f94
Chase Manning <chase@manning.dev>
2024-09-25 10:49:03 +0100

    ✅ add tests

---
+++ functions/src/__tests__/supply.test.ts
@@ -0,0 +1,21 @@
+import { getCirculatingSupply, getTotalSupply, getMaxSupply } from "../supply";
+import { MAX_SUPPLY } from "../constants";
+
+describe("Supply Functions", () => {
+  test("getCirculatingSupply should return a number", async () => {
+    const circulatingSupply = await getCirculatingSupply();
+    expect(typeof circulatingSupply).toBe("number");
+    expect(circulatingSupply).toBeGreaterThan(0);
+    expect(circulatingSupply).toBeLessThan(MAX_SUPPLY);
+  });
+
+  test("getTotalSupply should return MAX_SUPPLY", async () => {
+    const totalSupply = await getTotalSupply();
+    expect(totalSupply).toBe(MAX_SUPPLY);
+  });
+
+  test("getMaxSupply should return MAX_SUPPLY", async () => {
+    const maxSupply = await getMaxSupply();
+    expect(maxSupply).toBe(MAX_SUPPLY);
+  });
+});
